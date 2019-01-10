fe7e3c3e0ceb8cf48ecfe55a69ee79d4ffa976e1
Thota Surendra Babu <sthota@paypal.com>
2019-01-10 21:09:49 +0800

    added shared preference

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
-import 'introview.dart';
+import 'package:splash_intro/utilsconfig.dart';
+import 'package:splash_intro/introview.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:splash_intro/homescreen.dart';
+import 'dart:async';
 
 void main(){
   runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: new MyApp(),
   ));
 }
+
 class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
@@ -16,15 +22,15 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return new SplashScreen(
       seconds: 5,
-      navigateAfterSeconds: IntroView(),
+      navigateAfterSeconds: AfterSplash(),
       title: Text(
-        "App Name", 
+        splashScreenAppName, 
         style: TextStyle(
           fontSize: 30.0,
           fontWeight: FontWeight.bold
         ),
       ),
-      image: new Image.network("https://www.gown.io/templates/fsbp-evolutionstore/images/logo.png"),
+      image: new Image.network(splashScreenLogo),
       backgroundColor: Colors.teal,
       styleTextUnderTheLoader: new TextStyle(),
       loaderColor: Colors.white,
@@ -32,3 +38,36 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
+class AfterSplash extends StatefulWidget {
+  _AfterSplashState createState() => _AfterSplashState();
+}
+
+
+
+class _AfterSplashState extends State<AfterSplash> {
+  
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (_seen) {
+    Navigator.of(context).pushReplacement(
+        new MaterialPageRoute(builder: (context) => new HomeScreen()));
+    } else {
+    prefs.setBool('seen', true);
+    Navigator.of(context).pushReplacement(
+        new MaterialPageRoute(builder: (context) => new IntroView()));
+    }
+  }
+
+  @override
+  void initState() {
+      super.initState();
+      checkFirstSeen();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Offstage(child: null);
+  }
+}
---
+++ lib/utilsconfig.dart
@@ -0,0 +1,3 @@
+
+final String splashScreenLogo = "https://www.gown.io/templates/fsbp-evolutionstore/images/logo.png";
+final String splashScreenAppName = "App Name";
